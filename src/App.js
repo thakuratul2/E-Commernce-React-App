23ea086adf5eaafa6e9f33e070ae15f9aaf8da2c
thakuratul2 <pratapsinghatul111@gmail.com>
2023-03-12 10:58:18 +0530

    Initial Setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import Footer from './components/Footer'
+import Header from './components/Header'
 
 function App() {
   return (
-    <div>App</div>
+    <>
+<div>App</div>
+    <Header/>
+    <Footer/>
+    </>
+    
   )
 }
 
