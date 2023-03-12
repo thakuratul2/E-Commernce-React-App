e618d55654046f667bd500c38bba603584a31754
thakuratul2 <pratapsinghatul111@gmail.com>
2023-03-12 12:03:19 +0530

    Header Start

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Home from './pages/Home'
 import About from './pages/About';
 import Contact from './pages/Contact';
+import './App.css'
 import { BrowserRouter as Router, Routes,Route } from 'react-router-dom'
 import Layout from './components/Layout'
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import { NavLink,Link } from 'react-router-dom'
 
 function Header() {
   return (
-    <div>Header</div>
+    <Fragment>
+    <header className="header-top-strip py-3">
+    <div className="container-xxl">
+      <div className="row">
+        <div className="col-6">
+          <p className='text-white mb-0'>Free Shipping Available Over Rs. 399 & Free Return</p>
+        </div>
+        <div className="col-6">
+<p className=' text-end text-white mb-0'>
+Customer Care:
+  <a className='text-white' href="tel: +91 8192002404"> +91 8192002404</a>
+</p>
+        </div>
+      </div>
+    </div>
+
+    </header>
+    </Fragment>
   )
 }
 
