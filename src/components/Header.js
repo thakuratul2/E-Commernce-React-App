d85311b7e9b6c3c8e9f8a31902571aac929af15c
thakuratul2 <pratapsinghatul111@gmail.com>
2023-03-20 12:47:47 +0530

    Options Start

--- src/components/Header.js
+++ src/components/Header.js
@@ -23,21 +23,55 @@ Customer Care:
     </header>
     <header className="header-upper py-3">
 <div className="container-xxl">
-  <div className="row">
+  <div className="row align-items-center">
     <div className="col-2">
       <h6 className='header-text'>
         <Link className='text-link'>E-Commernce | Atul Pratap Singh</Link>
       </h6>
     </div>
     <div className="col-5">
-    <div className="input-group mb-3">
+    <div className="input-group">
   <input type="text" className="form-control py-2" placeholder="Search Product Here" aria-label="Search Product Here" aria-describedby="basic-addon2"/>
   <span className="input-group-text p-3" id="basic-addon2"><BsSearch className='fs-6'/></span>
 </div>
 
     </div>
     <div className="col-5">
+<div className='header-upprt-links d-flex align-items-center justify-content-between'>
+<div>
+  <Link>
+    <img src="images/compare.svg" className='options' alt="compare" />
+    <p>
+      Compare <br/> Products
+    </p>
+  </Link>
+</div>
+<div>
+  <Link>
+    <img src="images/wishlist.svg" className='options' alt="wishlist" />
+  </Link>
 
+  <p>
+
+  </p>
+</div>
+<div>
+  <Link>
+    <img src="" alt="" />
+  </Link>
+  <p>
+
+  </p>
+</div>
+<div>
+  <Link>
+    <img src="" alt="" />
+  </Link>
+  <p>
+
+  </p>
+</div>
+</div>
     </div>
   </div>
 </div>
